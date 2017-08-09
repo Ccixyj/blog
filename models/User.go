920099b630988219bf92fa984946bb628e35df82
徐宇健 <751762476@qq.com>
2017-08-09 15:31:33 +0800

    add admin user

--- models/User.go
+++ models/User.go
@@ -3,13 +3,62 @@ package models
 import (
 	"time"
 
+	"crypto/md5"
+	"fmt"
+
+	"crypto/sha512"
+	"encoding/base64"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"math/rand"
+	"strings"
 )
 
+type Password string
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func (pwd Password) GenerateSalt() string {
+	h := md5.New()
+	if _, ok := h.Write([]byte(string(pwd))); ok != nil {
+		fmt.Errorf("an error occurred : %v", ok)
+	}
+	md5key := fmt.Sprintf("%x", h.Sum(nil))
+	saltLen := r.Intn(5) + 5 //salt length
+	var strs = make([]byte, saltLen+1)
+	strLen := len(md5key)
+	for i := 0; i <= saltLen; i++ {
+		strs[i] = md5key[r.Intn(strLen)]
+	}
+	return string(strs)
+}
+
+func (pwd Password) doCompare(compare Password) bool {
+	decoded, error := base64.URLEncoding.DecodeString(string(compare))
+	if error != nil {
+		fmt.Errorf("decode base64 error %v", error)
+	}
+	splits := strings.Split(string(decoded), "$")
+	if len(splits) != 2 {
+		fmt.Errorf("decode base64 size must == 2 : %v", error)
+	}
+	return splits[1] == string(generateHash(splits[0], pwd))
+}
+
+//Password(base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%s$%s", salt, pwdStr))))
+func generateHash(salt string, oldPwd Password) string {
+	sh := sha512.New()
+	if _, ok := sh.Write([]byte(salt + string(oldPwd))); ok != nil {
+		fmt.Errorf("an error occurred : %v", ok)
+	}
+	bs := sh.Sum(nil)
+	return fmt.Sprintf("%x", bs)
+}
+
 type User struct {
-	ID      int64     `orm:"column(id)"`
-	Name    string    `orm:"size(20)"`
-	Pwd     string    `orm:"size(50)"`
+	ID      int64  `orm:"column(id)"`
+	Name    string `orm:"size(20)"`
+	Pwd     Password
 	Profile *Profile  `orm:"rel(fk);null;rel(one);on_delete(set_null)"`
 	Create  time.Time `orm:"auto_now_add;type(datetime)"`
 	Update  time.Time `orm:"auto_now;type(datetime)"`
@@ -24,5 +73,26 @@ type Profile struct {
 }
 
 func init() {
+	var admin = new(User)
+	admin.Name = "15152278073"
+	pwd := Password("45127996wind")
+	salt := pwd.GenerateSalt()
+	pwdStr := fmt.Sprintf("%x", sha512.New().Sum([]byte(salt+string(pwd))))
+	admin.Pwd = Password(fmt.Sprintf("%s$%s", salt, pwdStr))
+
+	o := orm.NewOrm()
+	_, e := o.Insert(admin)
+	if e != nil {
+		beego.Warn("%v is exit in database", admin.Name)
+	}
 	orm.RegisterModel(new(User), new(Profile))
+}
+
+func generateAdmin() *User {
+	var adamin = new(User)
+	adamin.Name = "15152278073"
+	pwd := Password("45127996wind")
+	salt := pwd.GenerateSalt()
+	adamin.Pwd = Password(base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%s$%s", salt, generateHash(salt, pwd)))))
+	return adamin
 }
---
+++ models/User_test.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSalt(t *testing.T) {
+	var pwd Password = "45127996wind"
+	var ss = ""
+	for len(ss) != 11 {
+		ss = pwd.GenerateSalt()
+		fmt.Println(ss)
+	}
+
+}
+
+func TestUser(t *testing.T) {
+	admin := generateAdmin()
+	fmt.Println(admin)
+
+	fmt.Println(Password("45127996").doCompare(admin.Pwd))
+	fmt.Println(Password("wind").doCompare(admin.Pwd))
+	fmt.Println(Password("45127996wind").doCompare(admin.Pwd))
+}
